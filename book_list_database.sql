eb40f0159a35ac38b9372724b4950f08dc73d16c
tlr-s <103860549+tlr-s@users.noreply.github.com>
2022-04-23 17:48:10 -0700

    Add files via upload

---
+++ book_list_database.sql
@@ -0,0 +1,38 @@
+/*Step 1
+In this first step, create a table to store your list of books. It should have columns for id, name, author, and rating.*/
+
+CREATE TABLE books (ID INTEGER PRIMARY KEY, name TEXT, author TEXT, rating REAL);
+
+/*Step 2
+Add books into the table.*/
+
+INSERT INTO books VALUES(1, "The Locked Door", "Freida McFadden", 10);
+INSERT INTO books VALUES(2, "Dream Town", "David Baldacci", 9.5);
+INSERT INTO books VALUES(3, "Run, Rose, Run: A Novel", "James Patterson", 9);
+INSERT INTO books VALUES(4, "The Golden Couple: A Novel", "Greer Hendricks", 8.5);
+INSERT INTO books VALUES(5, "At the Quiet Edge: A Novel", "Victoria Helen Stone", 8);
+INSERT INTO books VALUES(6, "The Investigator", "John Sandford", 7.5);
+INSERT INTO books VALUES(7, "Verity", "Colleen Hoover", 7);
+INSERT INTO books VALUES(8, "The Paris Apartment: A Novel", "Lucy Foley", 6.5);
+INSERT INTO books VALUES(9, "City on Fire: A Novel", "Don Winslow", 6);
+INSERT INTO books VALUES(10, "Strangers We Know", "Elle Marr", 5.5);
+INSERT INTO books VALUES(11, "No Middle Name", "Lee Child", 5);
+INSERT INTO books VALUES(12, "The Unknown Beloved: A Novel", "Amy Harmon", 4.5);
+INSERT INTO books VALUES(13, "The Arrangement", "Kiersten Modglin", 4);
+INSERT INTO books VALUES(14, "The Moonlight Child", "Karen McQuestion", 3.5);
+INSERT INTO books VALUES(15, "The Girl in Cabin 13", "A.J. Rivers", 3);
+INSERT INTO books VALUES(16, "The Target", "Catherine Coulter", 2.5);
+INSERT INTO books VALUES(17, "What Happened to the Bennetts", "Lisa Scottoline", 2);
+INSERT INTO books VALUES(18, "One By One", "Freida McFadden", 1.5);
+INSERT INTO books VALUES(19, "The Last Thing He Told Me: A Novel", "Laura Dave", 1);
+INSERT INTO books VALUES(20, "Or Else: A Thriller", "Joe Hart", 0.5);
+INSERT INTO books VALUES(21, "The Patient's Secret: A Novel", "Loreth Anne White", 0);
+INSERT INTO books VALUES(22, "The Maid: A Novel", "Nita Prose", 6);
+INSERT INTO books VALUES(23, "The Rose Code: A Novel", "Kate Quinn", 8);
+INSERT INTO books VALUES(24, "Death of the Black Widow", "James Patterson", 2);
+INSERT INTO books VALUES(25, "The House on the Hill", "Elle Gray", 4);
+INSERT INTO books VALUES(26, "The Wife Upstairs", "Freida McFadden", 8);
+INSERT INTO books VALUES(27, "Mother's Day", "Patricia MacDonald", 1);
+INSERT INTO books VALUES(28, "The Half-Life of Ruby Fielding: A Novel", "Lydia Kang", 4.5);
+INSERT INTO books VALUES(29, "Layla", "Colleen Hoover", 9.5);
+INSERT INTO books VALUES(30, "No Way Back", "J. B. Turner", 1.5);
